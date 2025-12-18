630ff6d2727ebbdf943261a68021c3df1db38a56
DinosaursAreCute <noah.huelskaemper@gmx.de>
2025-12-18 20:14:48 +0100

    Changed: added function to move file

--- ioHelper/fileOperations.py
+++ ioHelper/fileOperations.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import *
-
+import shutil
 import Utils.Logger
 
 # ======================== Initialization ========================
@@ -62,7 +62,8 @@ def list_files(root_path):
     log.debug(f"found {len(files)} files under {root}")
     return files
 
-def check_file_exists(file_path: Path) -> bool:
+def check_file_exists(file_path) -> bool:
+    file_path = file_path if isinstance(file_path, Path) else Path(file_path)
     if not file_path.exists():
         log.debug(f"File does not exist at path: {file_path}")
         return False
@@ -73,7 +74,7 @@ def check_file_exists(file_path: Path) -> bool:
 
 def delete_file(file_path) -> bool:
     file_path = file_path if isinstance(file_path, Path) else Path(file_path)
-    log.debug(f"Trying to remove file: {file_path}")
+    log.info(f"Trying to remove file: {file_path}")
     try:
         file_path.unlink()
     except FileNotFoundError:
@@ -88,17 +89,49 @@ def delete_file(file_path) -> bool:
 
 def create_file(file_path,replace_existing: bool = False)-> bool :
     file_path = file_path if isinstance(file_path, Path) else Path(file_path)
-    log.debug(f"Trying to create file: {file_path}")
+    log.info(f"Trying to create file: {file_path}")
     if file_path.exists() and not replace_existing:
         log.error(f"File: {file_path} already exists and replace files is: False.")
         return False
     if file_path.exists():
         log.info(f"File: {file_path} already exists and will be replaced")
-
     try:
         file_path.touch()
     except Exception as e:
         log.error(f"Failed to create file due to: {e}")
         return False
     log.success(f"File: {file_path} was successfully replaced")
+    return True
+
+def move_file(source_file_path,target_file_path,replace_existing_target_file: bool = False,create_file_if_not_exist: bool = False) -> bool:
+    source_file_path = source_file_path if isinstance(source_file_path, Path) else Path(source_file_path)
+    target_file_path= target_file_path if isinstance(target_file_path, Path) else Path(target_file_path)
+    log.info(f"Attempting to move file: {source_file_path} to {target_file_path}")
+    source_file_exists= check_file_exists(source_file_path)
+    target_file_exists= check_file_exists(target_file_path)
+    if source_file_path.is_dir():
+        log.error(f"Source file: {source_file_path} is a directory")
+        return False
+    if target_file_path.is_dir():
+        log.error(f"target file: {target_file_path} is a directory")
+    if not source_file_exists and not create_file_if_not_exist:
+        log.error(f"File {source_file_path} does not exist and create_file_if_not_exist is: {create_file_if_not_exist}")
+        return False
+    if target_file_exists and not replace_existing_target_file:
+        log.error(f"file {source_file_path.parts[-1]} already exists at {target_file_path} and replace_existing_target_file is: {replace_existing_target_file}")
+        return False
+    if target_file_exists:
+        log.info(f"File: {source_file_path.parts[-1]} already exists at target path: {target_file_path} and will be overwritten")
+    if not source_file_exists:
+        log.info(f"File: {source_file_path.parts[-1]} does not exist and will be created at filepath: {target_file_path}")
+    try:
+        shutil.move(str(source_file_path), str(target_file_path))
+    except WindowsError:
+        create_file(target_file_path, replace_existing_target_file)
+    except Exception as e:
+        log.error(f"Failed to move file: {source_file_path} -> {target_file_path}")
+    if source_file_path.exists() or not target_file_path.exists():
+        log.error(f"Failed to move file: {source_file_path} to: {target_file_path}")
+        return False
+    log.success(f"Successfully moved file from {source_file_path} to: {target_file_path}")
     return True
--- main.py
+++ main.py
@@ -10,11 +10,11 @@ import ioHelper.fileOperations as fOps
 if __name__ == "__main__":
 
     log1 = Logger.LoggerClass(logger_name="TestLogger",logger_level = 1,log_color = True)
-    path = fOps.convert_string_to_path("Test/Test_1/Test_1_file.txt")
+    path = fOps.convert_string_to_path("Test/Test_1/Test_2_file.txt")
     subdirMap=fOps.list_subdirectories(path,True)
     for x in subdirMap:
         files=fOps.list_files(x)
 
 
-    fOps.create_file(path)
-    fOps.check_file_exists(path)
+
+    fOps.move_file(path,f"Test/{path.parts[-1]}",True,True)
